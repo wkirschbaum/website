0af1bcc54f08061b40fca196344719402ad40837
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2014-12-24 09:04:24 +0200

    update package versions

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.3"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler wilhelmblog.core.handler/app}
